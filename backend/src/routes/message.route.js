6c79c9f1cf72022c434be636ddee4d0a0c9a1dc2
Nischal Puri <mrnischalpuri@gmail.com>
2025-10-06 08:13:12 +0545

    arject Protection and route protection middleware added in message route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -6,19 +6,26 @@ import {
   sendMessageController,
 } from "../controllers/message.controller.js"
 import { protectRoute } from "../middleware/auth.middleware.js"
+import { arcjectProtection } from "../middleware/arcjet.middleware.js"
 
 const router = express.Router()
 
+// Apply Arcjet protection middleware for rate limiting and security and then apply authentication middleware
+// Apply protection middleware to all routes in this router
+// This ensures that only authenticated users can access these routes
+
+router.use(arcjectProtection, protectRoute)
+
 // Get all contacts
-router.get("/contacts", protectRoute, getAllContactsController)
+router.get("/contacts", getAllContactsController)
 
 // Get chat partner details
-router.get("/chats", protectRoute, getChatPartnerController)
+router.get("/chats", getChatPartnerController)
 
 // Get messages with a specific contact
-router.get("/:id", protectRoute, getMessagesController)
+router.get("/:id", getMessagesController)
 
 // Send a new message
-router.post("/send/:id", protectRoute, sendMessageController)
+router.post("/send/:id", sendMessageController)
 
 export default router
